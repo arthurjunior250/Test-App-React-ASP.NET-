3a5ffbf9ff652200fe12497c558e35c68de42d1c
arthurjunior250 <arthurjunior88741@gmail.com>
2025-02-04 10:44:46 +0200

    fix/b

--- frontend/src/components/config.js
+++ frontend/src/components/config.js
@@ -1,2 +1,2 @@
 // export const BASE_URL = "http://localhost:5000";
-export const BASE_URL = "https://test-app-react-asp-net.onrender.com/";
+export const BASE_URL = "https://test-app-react-asp-net.onrender.com";
